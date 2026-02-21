eacbf9819f36ae21ef56521779ec09d40258d1c8
Arsalan Javed <arsalanjaved@Arsalans-MacBook-Pro.local>
2026-02-21 00:07:29 -0600

    fix: exclude sitemap from auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,9 +13,10 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
+         * - sitemap.xml (sitemap file)
          * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
